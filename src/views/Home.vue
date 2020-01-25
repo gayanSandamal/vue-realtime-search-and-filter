217c6c3212758db664d8c8325dc3a02937df9593
gayan_nayague <gayan.sandamal@outlook.com>
2020-01-26 01:48:42 +0530

    1. bug fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -268,11 +268,12 @@ export default {
           // loop through matching values
             this.organizations.map(q => {
               if (p.organization_id === q._id) {
+                debugger
                 // declare to search result
                 search_result.push({
                   organization_name: q.name,
                   ticket_subject: p.subject,
-                  username: q.name
+                  username: o.name
                 })
               }
             })
